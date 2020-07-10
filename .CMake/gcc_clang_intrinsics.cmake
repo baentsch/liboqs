029dcab83c679f0401639391cf746ea103a5543c
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-07-10 12:36:48 +0200

    ppc/x64 switch

--- .CMake/gcc_clang_intrinsics.cmake
+++ .CMake/gcc_clang_intrinsics.cmake
@@ -2,7 +2,11 @@
 
 try_run(RUN_RESULT COMPILE_RESULT
         "${CMAKE_BINARY_DIR}" "${PROJECT_SOURCE_DIR}/.CMake/detect_gcc_clang_intrinsics.c"
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "ppc64le")
         COMPILE_DEFINITIONS -mcpu=native
+else()
+        COMPILE_DEFINITIONS -march=native
+endif()
         RUN_OUTPUT_VARIABLE RUN_OUTPUT)
 if(NOT RUN_RESULT EQUAL 0)
      message(FATAL_ERROR ".CMake/detect_gcc_clang_intrinsics.c returned exit code/output: " ${RUN_RESULT} ${RUN_OUTPUT})
