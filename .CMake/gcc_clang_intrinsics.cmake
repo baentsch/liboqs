f7c95bdfb861bd543ac420573b4b08d8e5b038e2
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-07-10 12:29:53 +0200

    tune for ppc64le

--- .CMake/gcc_clang_intrinsics.cmake
+++ .CMake/gcc_clang_intrinsics.cmake
@@ -2,7 +2,7 @@
 
 try_run(RUN_RESULT COMPILE_RESULT
         "${CMAKE_BINARY_DIR}" "${PROJECT_SOURCE_DIR}/.CMake/detect_gcc_clang_intrinsics.c"
-        COMPILE_DEFINITIONS -march=native
+        COMPILE_DEFINITIONS -mcpu=native
         RUN_OUTPUT_VARIABLE RUN_OUTPUT)
 if(NOT RUN_RESULT EQUAL 0)
      message(FATAL_ERROR ".CMake/detect_gcc_clang_intrinsics.c returned exit code/output: " ${RUN_RESULT} ${RUN_OUTPUT})
